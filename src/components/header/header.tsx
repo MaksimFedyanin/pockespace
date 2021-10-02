033215b6c1d70b9aac057ac08040c4be5526abe0
Максим Федянин <maxim.fedyanin@pladform.ru>
2021-10-02 14:45:45 +0300

    попап

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,41 +1,41 @@
 // @generated: @expo/next-adapter@2.1.52
-import React from "react";
-import { StyleSheet, View, Image } from "react-native";
-import Wrapper from "../wrapper/wrapper";
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import Wrapper from '../wrapper/wrapper';
 
-const Header = ({ onLayout = () => ({}) }) => {
-    return <View style={styles.header} onLayout={onLayout}>
-        <Wrapper>
-            <View style={styles.info}>
-                {/*<IconButton name='menu-outline' onPress={() => ({})}/>*/}
-                <Image
-                    style={styles.logo}
-                    source={{
-                        uri: 'https://see.fontimg.com/api/renderfont4/DO3zx/eyJyIjoiZnMiLCJoIjoxMzAsInciOjIwMDAsImZzIjo2NSwiZmdjIjoiIzAwMDAwMCIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/QnJlYWtpbmcgU3BhY2U/magland.png',
-                    }}
-                    resizeMode="contain"
-                />
-                {/*<IconButton name='message-circle-outline' onPress={() => ({})}/>*/}
-            </View>
-            <View/>
-        </Wrapper>
-    </View>;
-};
+const Header = ({ onLayout = () => ({}) }) => (
+  <View style={styles.header} onLayout={onLayout}>
+    <Wrapper>
+      <View style={styles.info}>
+        {/* <IconButton name='menu-outline' onPress={() => ({})}/> */}
+        <Image
+          style={styles.logo}
+          source={{
+            uri: 'https://see.fontimg.com/api/renderfont4/DO3zx/eyJyIjoiZnMiLCJoIjoxMzAsInciOjIwMDAsImZzIjo2NSwiZmdjIjoiIzAwMDAwMCIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/QnJlYWtpbmcgU3BhY2U/magland.png',
+          }}
+          resizeMode="contain"
+        />
+        {/* <IconButton name='message-circle-outline' onPress={() => ({})}/> */}
+      </View>
+      <View />
+    </Wrapper>
+  </View>
+);
 
 const styles = StyleSheet.create({
-    header: {
-        width: '100%',
-        height: 75,
-    },
-    info: {
-        flexDirection: "row",
-        justifyContent: "center",
-        width: '100%',
-    },
-    logo: {
-        width: 200,
-        height: 40,
-    }
+  header: {
+    width: '100%',
+    height: 75,
+  },
+  info: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  logo: {
+    width: 200,
+    height: 40,
+  },
 });
 
 export default Header;
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,59 +1,67 @@
 // @generated: @expo/next-adapter@2.1.52
 import React from 'react';
-import {Pressable, StyleSheet, Text, View} from "react-native";
-import IconButton from "../icon-button/icon-button";
-import Wrapper from "../wrapper/wrapper";
+import {
+  Image,
+  Pressable, StyleSheet, Text, View,
+} from 'react-native';
+import IconButton from '../icon-button/icon-button';
+import Wrapper from '../wrapper/wrapper';
 
-const Menu = () => {
-    return <Wrapper>
-        <View style={styles.menu}>
-            <Text style={styles.logo}>POCKESPACE</Text>
-            <View style={styles.items}>
-                <Pressable style={styles.item} accessibilityRole="button">
-                    <IconButton name='home-outline' fill="rgb(144, 144, 144)" onPress={() => ({})}/>
-                    <Text style={styles.title}>Home</Text>
-                </Pressable>
-                <Pressable style={styles.item} accessibilityRole="button">
-                    <IconButton name='search-outline' fill="rgb(144, 144, 144)" onPress={() => ({})}/>
-                    <Text style={styles.title}>Search</Text>
-                </Pressable>
-                <Pressable style={styles.item} accessibilityRole="button">
-                    <IconButton name='smartphone-outline' fill="rgb(144, 144, 144)" onPress={() => ({})}/>
-                    <Text style={styles.title}>Feed</Text>
-                </Pressable>
-                <Pressable style={styles.item} accessibilityRole="button">
-                    <IconButton name='bell-outline' fill="rgb(144, 144, 144)" onPress={() => ({})}/>
-                    <Text style={styles.title}>Notification</Text>
-                </Pressable>
-                <Pressable style={styles.item} accessibilityRole="button">
-                    <IconButton name='person-outline' fill="rgb(144, 144, 144)" onPress={() => ({})}/>
-                    <Text style={styles.title}>Profile</Text>
-                </Pressable>
-            </View>
-        </View>
-    </Wrapper>
-};
+const Menu = () => (
+  <Wrapper>
+    <View style={styles.menu}>
+      <Image
+        style={styles.logo}
+        source={{
+          uri: 'https://see.fontimg.com/api/renderfont4/DO3zx/eyJyIjoiZnMiLCJoIjoxMzAsInciOjIwMDAsImZzIjo2NSwiZmdjIjoiIzAwMDAwMCIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/QnJlYWtpbmcgU3BhY2U/magland.png',
+        }}
+        resizeMode="contain"
+      />
+      <View style={styles.items}>
+        <Pressable style={styles.item} accessibilityRole="button">
+          <IconButton name="home-outline" fill="rgb(144, 144, 144)" onPress={() => ({})} />
+          <Text style={styles.title}>Home</Text>
+        </Pressable>
+        <Pressable style={styles.item} accessibilityRole="button">
+          <IconButton name="search-outline" fill="rgb(144, 144, 144)" onPress={() => ({})} />
+          <Text style={styles.title}>Search</Text>
+        </Pressable>
+        <Pressable style={styles.item} accessibilityRole="button">
+          <IconButton name="smartphone-outline" fill="rgb(144, 144, 144)" onPress={() => ({})} />
+          <Text style={styles.title}>Feed</Text>
+        </Pressable>
+        <Pressable style={styles.item} accessibilityRole="button">
+          <IconButton name="bell-outline" fill="rgb(144, 144, 144)" onPress={() => ({})} />
+          <Text style={styles.title}>Notification</Text>
+        </Pressable>
+        <Pressable style={styles.item} accessibilityRole="button">
+          <IconButton name="person-outline" fill="rgb(144, 144, 144)" onPress={() => ({})} />
+          <Text style={styles.title}>Profile</Text>
+        </Pressable>
+      </View>
+    </View>
+  </Wrapper>
+);
 
 const styles = StyleSheet.create({
-    logo: {
-        color: 'rgb(72, 72, 72)',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    menu: {
-        justifyContent: "flex-start",
-    },
-    items: {
-        marginTop: 40,
-    },
-    item: {
-        flexDirection: "row",
-        width: '100%',
-        height: 40,
-    },
-    title: {
-        marginLeft: 18,
-    },
+  logo: {
+    width: 150,
+    height: 40,
+  },
+  menu: {
+    justifyContent: 'flex-start',
+  },
+  items: {
+    marginTop: 40,
+  },
+  item: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 40,
+  },
+  title: {
+    marginLeft: 18,
+  },
 });
 
 export default Menu;
